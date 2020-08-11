9cfb83b38d6ec867b325070d464d1430ecd9072d
Anat <anat.hashavit@gmail.com>
2020-08-11 17:48:32 +0300

    Grid tests from search game

--- games/search/Grid.py
+++ games/search/Grid.py
@@ -1,6 +1,6 @@
 import copy
 from enum import Enum
-
+from itertools import product
 
 ####################################################
 # Beging of the game:
@@ -30,12 +30,15 @@ class Actions(Enum):
 
 
 ATTACKER_TRANSITIONS = {(0,1):[Actions.NORTH_EAST, Actions.EAST, Actions.SOUTH_EAST],
-                        (1,0):[Actions.STAY, Actions.EAST, Actions.NORTH],
-                        (1,1):[Actions.STAY, Actions.EAST],
-                        (1,2):[Actions.STAY, Actions.EAST, Actions.SOUTH],
-                        (2,0):[Actions.STAY, Actions.EAST],
+                        (1,0): [Actions.STAY, Actions.EAST],
+                        (1,1): [Actions.STAY, Actions.EAST],
+                        (1,2): [Actions.STAY, Actions.EAST],
+                        (2,0):[Actions.STAY, Actions.EAST, Actions.NORTH],
                         (2,1):[Actions.STAY, Actions.EAST],
-                        (2,2):[Actions.STAY, Actions.EAST],
+                        (2,2):[Actions.STAY, Actions.EAST, Actions.SOUTH],
+                        (3,0):[Actions.STAY, Actions.EAST],
+                        (3,1):[Actions.STAY, Actions.EAST],
+                        (3,2):[Actions.STAY, Actions.EAST],
 }
 
 SUCCESFULL_ATTACK_PAYOFF = {(3,0):3,
@@ -44,33 +47,35 @@ SUCCESFULL_ATTACK_PAYOFF = {(3,0):3,
 
 
 class Node:
-    def __init__(self, occupants = [], tracks = False, payoff = 0):
+    def __init__(self, occupants, tracks = False, payoff = None):
         self.occupants = occupants
         self.tracks = tracks
         self.payoff = payoff
 
 INITIAL_GRID = {
-    (0,1): Node([OCCUPANTS.ATTACKER], None, 0),
-    (1,0): Node(),
+    (0,1): Node([OCCUPANTS.ATTACKER]),
+    (1,0): Node([]),
     (1,1): Node([OCCUPANTS.P1]),
-    (1,2): Node(),
-    (2,0): Node(),
-    (2,1): Node([OCCUPANTS.P2]),
-    (2,2): Node(),
-    (3,0): Node([], None, 3),
-    (3,1): Node([], None, 5),
-    (3,2): Node([], None, 10)
+    (1,2): Node([]),
+    (2,0): Node([]),
+    (2,1): Node([]),
+    (2,2): Node([]),
+    (3,0): Node([]),
+    (3,1): Node([OCCUPANTS.P2]),
+    (3,2): Node([]),
+    (4,0): Node([], False, 3),
+    (4,1): Node([], False, 10),
+    (4,2): Node([], False, 5)
 }
-
 
 class Grid:
     def __init__(self, attacker_goal = None, matrix = INITIAL_GRID,
                  attacker_location = (0,1),
                  p1_location = (1,1),
-                 p2_location = (2,1)):
+                 p2_location = (3,1)):
 
-        self.attacker_location = attacker_location
         self.attacker_goal = attacker_goal
+        self.locations = {}
         self.locations[OCCUPANTS.ATTACKER] = attacker_location
         self.locations[OCCUPANTS.P1] = p1_location
         self.locations[OCCUPANTS.P2] = p2_location
@@ -78,54 +83,52 @@ class Grid:
 
     def set_attacker_goal(self, goal):
         new_grid = copy.deepcopy(self)
+        if goal[0] != MAX_X:
+            raise ValueError
         new_grid.attacker_goal = goal
         return new_grid
 
-    def apply_attacker_action(self, action):
-        new_grid = copy.deepcopy(self)
-        new_grid.__execute_attacker_action(action)
-        return new_grid
-
-    def apply_defender_action(self, p1_action, p2_action):
-        new_grid = copy.deepcopy(self)
-        new_grid.__execute_defender_action(p1_action, p2_action)
-        return new_grid
-
     def get_attacker_actions(self):
-        (x,y) = (self.attacker_location.x_loc, self.attacker_location.y_loc)
+        (x,y) = (self.locations[OCCUPANTS.ATTACKER][0], self.locations[OCCUPANTS.ATTACKER][1])
         if not (x,y) in ATTACKER_TRANSITIONS:
             return []
-        return ATTACKER_TRANSITIONS[(self.attacker_location.x_loc, self.attacker_location.y_loc)]
+        return ATTACKER_TRANSITIONS[(x,y)]
 
     def get_patroller_actions(self, location):
         actions = []
+        if location[0] not in [1,3]:
+            raise ValueError
 
-        if location.y_loc < MAX_Y:  # up
+        if location[1] < MAX_Y:  # up
             #            actions.append((self.x_loc, self.y_loc + 1))
             actions.append(Actions.NORTH)
 
-        if location.y_loc > 0:  # down
+        if location[1] > 0:  # down
             #           actions.append((self.x_loc, self.y_loc - 1))
             actions.append(Actions.SOUTH)
 
         return actions
 
     def get_defender_actions(self):
-        p1_actions = self.p1_location.get_defender_actions()
-        p_2_actions = self.p1_location.get_defender_actions()
-        return list(zip(p1_actions, p_2_actions))
+        p1_actions = self.get_patroller_actions(self.locations[OCCUPANTS.P1])
+        p_2_actions = self.get_patroller_actions(self.locations[OCCUPANTS.P2])
+        prod = product(p1_actions, p_2_actions)
+        return list(prod)
 
     @staticmethod
     def get_new_location(current_x, current_y, action):
         new_x = current_x
         new_y = current_y
-        if action == Actions.EAST:
+        if action == Actions.STAY:
+            return current_x, current_y
+
+        elif action == Actions.EAST:
             new_x += 1
         elif action == Actions.NORTH:
-            new_y = +1
+            new_y += 1
 
         elif action == Actions.SOUTH:
-            new_y = -1
+            new_y -= 1
 
         elif action == Actions.NORTH_EAST:
            new_x += 1
@@ -137,21 +140,6 @@ class Grid:
 
         return new_x, new_y
 
-    def execute_patroller_action(self, current_x, current_y, action, patroller_id):
-        new_x, new_y = self.get_new_location(current_x,current_y, action)
-        self.matrix[(current_x, current_y)].matrix.remove(patroller_id)
-        self.matrix[(new_x, new_y)].occupants.append(patroller_id)
-
-    def update_matrix(self, occupant, action, tracks = False):
-        current_x = self.locations[occupant].x_loc
-        current_y = self.locations[occupant].y_loc
-        new_x, new_y = self.get_new_location(current_x, current_y, action)
-        self.matrix[(current_x, current_y)].matrix.remove(occupant)
-        self.matrix[(new_x, new_y)].occupants.append(occupant)
-        self.matrix[(new_x, new_y)].tracks = tracks
-        self.locations[occupant].x_loc = new_x
-        self.locations[occupant].y_loc = new_y
-
     def attacker_caught(self):
         return self.locations[OCCUPANTS.ATTACKER] == self.locations[OCCUPANTS.P1] or \
                self.locations[OCCUPANTS.ATTACKER] == self.locations[OCCUPANTS.P2]
@@ -161,19 +149,41 @@ class Grid:
         if self.locations[OCCUPANTS.ATTACKER] == self.locations[OCCUPANTS.P1]\
                 or self.locations[OCCUPANTS.ATTACKER] == self.locations[OCCUPANTS.P2]:
             return 0
-        # attacker reached a goal node
-        if self.attacker_location == self.attacker_location[OCCUPANTS.ATTACKER]:
-            x = self.attacker_location[OCCUPANTS.ATTACKER].x_loc
-            y = self.attacker_location[OCCUPANTS.ATTACKER].y_loc
+        # attacker reached its goal node
+        if self.attacker_goal == self.locations[OCCUPANTS.ATTACKER]:
+            x = self.locations[OCCUPANTS.ATTACKER][0]
+            y = self.locations[OCCUPANTS.ATTACKER][1]
             return self.matrix[(x,y)].payoff
 
-        if self.attacker_location[OCCUPANTS.ATTACKER].x_loc == MAX_X:
-            return inf
+        #reached a goal node not its own
+        if self.locations[OCCUPANTS.ATTACKER][0] == MAX_X:
+            return -inf
+
         return None
 
+    def update_matrix(self, occupant, action, tracks = False):
+        current_x = self.locations[occupant][0]
+        current_y = self.locations[occupant][1]
+        self.matrix[(current_x, current_y)].tracks = tracks
+
+        new_x, new_y = self.get_new_location(current_x, current_y, action)
+        self.matrix[(current_x, current_y)].occupants.remove(occupant)
+        self.matrix[(new_x, new_y)].occupants.append(occupant)
+        self.locations[occupant] = (new_x, new_y)
+
+    def apply_attacker_action(self, action):
+        new_grid = copy.deepcopy(self)
+        new_grid.__execute_attacker_action(action)
+        return new_grid
+
+    def apply_defender_action(self, p1_action, p2_action):
+        new_grid = copy.deepcopy(self)
+        new_grid.__execute_defender_action(p1_action, p2_action)
+        return new_grid
+
     def __execute_attacker_action(self, action):
-        current_x = self.attacker_location.x_loc
-        current_y = self.attacker_location.y_loc
+        current_x = self.locations[OCCUPANTS.ATTACKER][0]
+        current_y = self.locations[OCCUPANTS.ATTACKER][1]
         if action == Actions.STAY:
             self.matrix[(current_x, current_y)].tracks = False
         else:
@@ -182,11 +192,3 @@ class Grid:
     def __execute_defender_action(self, p1_action, p2_action):
         self.update_matrix(OCCUPANTS.P1, p1_action)
         self.update_matrix(OCCUPANTS.P2, p2_action)
-
-
-
-
-
-
-
-
---
+++ tests/games/search/GridTest.py
@@ -0,0 +1,226 @@
+import unittest
+from math import inf
+from search.Grid import Grid, Actions, OCCUPANTS, Node
+
+
+class TestGrid  (unittest.TestCase):
+
+    def test_set_attacker_goal_ok(self):
+        grid = Grid()
+        goal = (4,0)
+        self.assertIsNone(grid.attacker_goal)
+        goal_grid = grid.set_attacker_goal(goal)
+        self.assertEqual(goal, goal_grid.attacker_goal)
+        self.assertIsNone(grid.attacker_goal)
+
+    def test_set_attacker_goal_raises_error(self):
+        grid = Grid()
+        self.assertIsNone(grid.attacker_goal)
+        with self.assertRaises(ValueError):
+            grid.set_attacker_goal((2,0))
+
+    def test_get_attacker_actions(self):
+        grid = Grid()
+        self.assertCountEqual(grid.get_attacker_actions(), [Actions.NORTH_EAST, Actions.EAST, Actions.SOUTH_EAST])
+        grid.locations[OCCUPANTS.ATTACKER] = (1,0)
+        self.assertCountEqual(grid.get_attacker_actions(), [Actions.STAY, Actions.EAST])
+        grid.locations[OCCUPANTS.ATTACKER] = (1,1)
+        self.assertCountEqual(grid.get_attacker_actions(), [Actions.STAY, Actions.EAST])
+        grid.locations[OCCUPANTS.ATTACKER] = (1,2)
+        self.assertCountEqual(grid.get_attacker_actions(), [Actions.STAY, Actions.EAST])
+        grid.locations[OCCUPANTS.ATTACKER] = (2,0)
+        self.assertCountEqual(grid.get_attacker_actions(), [Actions.STAY, Actions.EAST, Actions.NORTH])
+        grid.locations[OCCUPANTS.ATTACKER] = (2,1)
+        self.assertCountEqual(grid.get_attacker_actions(), [Actions.STAY, Actions.EAST])
+        grid.locations[OCCUPANTS.ATTACKER] = (2,2)
+        self.assertCountEqual(grid.get_attacker_actions(), [Actions.STAY, Actions.EAST, Actions.SOUTH])
+        grid.locations[OCCUPANTS.ATTACKER] = (3,0)
+        self.assertCountEqual(grid.get_attacker_actions(), [Actions.STAY, Actions.EAST])
+        grid.locations[OCCUPANTS.ATTACKER] = (3,1)
+        self.assertCountEqual(grid.get_attacker_actions(), [Actions.STAY, Actions.EAST])
+        grid.locations[OCCUPANTS.ATTACKER] = (3,2)
+        self.assertCountEqual(grid.get_attacker_actions(), [Actions.STAY, Actions.EAST])
+        grid.locations[OCCUPANTS.ATTACKER] = (4,0)
+        self.assertCountEqual(grid.get_attacker_actions(), [])
+        grid.locations[OCCUPANTS.ATTACKER] = (4,1)
+        self.assertCountEqual(grid.get_attacker_actions(), [])
+        grid.locations[OCCUPANTS.ATTACKER] = (4,2)
+        self.assertCountEqual(grid.get_attacker_actions(), [])
+
+    def test_get_patroller_actions(self):
+        grid = Grid()
+        self.assertCountEqual(grid.get_patroller_actions((1, 0)), [Actions.NORTH])
+        self.assertCountEqual(grid.get_patroller_actions((1, 1)), [Actions.NORTH, Actions.SOUTH])
+        self.assertCountEqual(grid.get_patroller_actions((1, 2)), [Actions.SOUTH])
+        self.assertCountEqual(grid.get_patroller_actions((3, 0)), [Actions.NORTH])
+        self.assertCountEqual(grid.get_patroller_actions((3, 1)), [Actions.NORTH, Actions.SOUTH])
+        self.assertCountEqual(grid.get_patroller_actions((3, 2)), [Actions.SOUTH])
+
+    def test_get_patroller_actions_raises_error(self):
+            grid = Grid()
+            with self.assertRaises(ValueError):
+                grid.get_patroller_actions((2,0))
+
+    def test_get_defender_actions(self):
+        grid = Grid()
+        grid.locations[OCCUPANTS.P1] = (1, 0)
+        grid.locations[OCCUPANTS.P2] = (3, 0)
+        self.assertCountEqual(grid.get_defender_actions(), [(Actions.NORTH, Actions.NORTH)])
+        grid.locations[OCCUPANTS.P2] = (3, 1)
+        self.assertCountEqual(grid.get_defender_actions(), [(Actions.NORTH, Actions.NORTH),
+                                                            (Actions.NORTH, Actions.SOUTH)])
+        grid.locations[OCCUPANTS.P2] = (3, 2)
+        self.assertCountEqual(grid.get_defender_actions(), [(Actions.NORTH, Actions.SOUTH)])
+
+        grid.locations[OCCUPANTS.P1] = (1, 1)
+        grid.locations[OCCUPANTS.P2] = (3, 0)
+        self.assertCountEqual(grid.get_defender_actions(), [(Actions.NORTH, Actions.NORTH),
+                                                            (Actions.SOUTH, Actions.NORTH)])
+        grid.locations[OCCUPANTS.P2] = (3, 1)
+        self.assertCountEqual(grid.get_defender_actions(), [(Actions.NORTH, Actions.NORTH),
+                                                            (Actions.SOUTH, Actions.NORTH),
+                                                            (Actions.NORTH, Actions.SOUTH),
+                                                            (Actions.SOUTH, Actions.SOUTH)
+                                                            ])
+        grid.locations[OCCUPANTS.P2] = (3, 2)
+        self.assertCountEqual(grid.get_defender_actions(), [(Actions.NORTH, Actions.SOUTH),
+                                                            (Actions.SOUTH, Actions.SOUTH)])
+
+        grid.locations[OCCUPANTS.P1] = (1, 2)
+        grid.locations[OCCUPANTS.P2] = (3, 0)
+        self.assertCountEqual(grid.get_defender_actions(), [(Actions.SOUTH, Actions.NORTH)])
+        grid.locations[OCCUPANTS.P2] = (3, 1)
+        self.assertCountEqual(grid.get_defender_actions(), [(Actions.SOUTH, Actions.NORTH),
+                                                            (Actions.SOUTH, Actions.SOUTH)])
+        grid.locations[OCCUPANTS.P2] = (3, 2)
+        self.assertCountEqual(grid.get_defender_actions(), [(Actions.SOUTH, Actions.SOUTH)])
+
+
+    def test_get_new_location(self):
+        grid = Grid()
+        self.assertEqual(grid.get_new_location(2,2, Actions.NORTH_EAST), (3,3))
+        self.assertEqual(grid.get_new_location(2,2, Actions.SOUTH_EAST), (3,1))
+        self.assertEqual(grid.get_new_location(2,2, Actions.NORTH), (2,3))
+        self.assertEqual(grid.get_new_location(2,2, Actions.SOUTH), (2,1))
+        self.assertEqual(grid.get_new_location(2,2, Actions.EAST),  (3,2))
+        self.assertEqual(grid.get_new_location(2,2, Actions.STAY),  (2,2))
+
+    def test_attacker_caghut(self):
+        grid = Grid()
+        grid.locations[OCCUPANTS.ATTACKER] = (1, 1)
+        grid.locations[OCCUPANTS.P1] = (1, 1)
+        grid.locations[OCCUPANTS.P2] = (2, 1)
+        self.assertTrue(grid.attacker_caught())
+        grid.locations[OCCUPANTS.P1] = (1, 2)
+        self.assertFalse(grid.attacker_caught())
+        grid.locations[OCCUPANTS.P2] = (1, 1)
+        self.assertTrue(grid.attacker_caught())
+
+    def test_get_games_values(self):
+        grid = Grid()
+        self.assertEqual(grid.get_game_value(), None)
+        grid.locations[OCCUPANTS.ATTACKER] = (1, 1)
+        grid.locations[OCCUPANTS.P1] = (1, 1)
+        grid.locations[OCCUPANTS.P2] = (2, 1)
+        self.assertEqual(grid.get_game_value(), 0)
+        grid.locations[OCCUPANTS.ATTACKER] = (4, 0)
+        grid = grid.set_attacker_goal((4,0))
+        self.assertEqual(grid.get_game_value(), 3)
+        grid = grid.set_attacker_goal((4, 1))
+        self.assertEqual(grid.get_game_value(), -inf)
+        grid = grid.set_attacker_goal((4, 2))
+        self.assertEqual(grid.get_game_value(), -inf)
+        grid.locations[OCCUPANTS.ATTACKER] = (4, 1)
+        grid = grid.set_attacker_goal((4,0))
+        self.assertEqual(grid.get_game_value(), -inf)
+        grid = grid.set_attacker_goal((4, 1))
+        self.assertEqual(grid.get_game_value(), 10)
+        grid = grid.set_attacker_goal((4, 2))
+        self.assertEqual(grid.get_game_value(), -inf)
+        grid.locations[OCCUPANTS.ATTACKER] = (4, 2)
+        grid = grid.set_attacker_goal((4,0))
+        self.assertEqual(grid.get_game_value(), -inf)
+        grid = grid.set_attacker_goal((4, 1))
+        self.assertEqual(grid.get_game_value(), -inf)
+        grid = grid.set_attacker_goal((4, 2))
+        self.assertEqual(grid.get_game_value(), 5)
+
+    def test_update_matrix(self):
+        grid = Grid()
+        two_one = grid.matrix[(2, 1)]
+        zero_one = grid.matrix[(0, 1)]
+        self.assertCountEqual(zero_one.occupants , [OCCUPANTS.ATTACKER])
+        self.assertIsNone(zero_one.payoff)
+        self.assertFalse(zero_one.tracks)
+        #attacker moves from (0,1) to (1,1)
+        grid.update_matrix(OCCUPANTS.ATTACKER, Actions.EAST)
+        self.assertListEqual(zero_one.occupants,[])
+        self.assertIsNone(zero_one.payoff)
+        self.assertFalse(zero_one.tracks)
+        one_one = grid.matrix[(1, 1)]
+        self.assertCountEqual(one_one.occupants,[OCCUPANTS.ATTACKER, OCCUPANTS.P1])
+        self.assertIsNone(one_one.payoff)
+        self.assertFalse(one_one.tracks)
+
+        three_one = grid.matrix[(3, 1)]
+        self.assertCountEqual(three_one.occupants,[OCCUPANTS.P2])
+        three_zero = grid.matrix[(3, 0)]
+        self.assertCountEqual(three_zero.occupants, [])
+        #P2 moves from (3,1) t0 (3,0)
+
+        grid.update_matrix(OCCUPANTS.P2, Actions.SOUTH)
+        self.assertListEqual(three_one.occupants,[])
+        self.assertIsNone(three_one.payoff)
+        self.assertFalse(three_one.tracks)
+
+        self.assertCountEqual(three_zero.occupants,[OCCUPANTS.P2])
+        self.assertIsNone(three_zero.payoff)
+        self.assertFalse(three_zero.tracks)
+        # attacker moves from (1,1) to (2,1)
+        grid.update_matrix(OCCUPANTS.ATTACKER, Actions.EAST, True)
+        two_one = grid.matrix[(2, 1)]
+        self.assertListEqual(one_one.occupants,[OCCUPANTS.P1])
+        self.assertIsNone(one_one.payoff)
+        self.assertTrue(one_one.tracks)
+
+        self.assertCountEqual(two_one.occupants,[OCCUPANTS.ATTACKER])
+        self.assertIsNone(two_one.payoff)
+        self.assertFalse(two_one.tracks)
+
+    def test_apply_attacker_action(self):
+        grid = Grid()
+        new_grid = grid.apply_attacker_action(Actions.EAST)
+        self.assertEqual(grid.locations[OCCUPANTS.ATTACKER], (0,1))
+        self.assertEqual(grid.locations[OCCUPANTS.P1], (1,1))
+        self.assertEqual(grid.locations[OCCUPANTS.P2], (3,1))
+        self.assertListEqual(grid.matrix[(0, 1)].occupants, [OCCUPANTS.ATTACKER])
+        self.assertListEqual(grid.matrix[(1, 1)].occupants, [OCCUPANTS.P1])
+        self.assertListEqual(grid.matrix[(3, 1)].occupants, [OCCUPANTS.P2])
+
+        self.assertEqual(new_grid.locations[OCCUPANTS.ATTACKER], (1,1))
+        self.assertEqual(new_grid.locations[OCCUPANTS.P1], (1,1))
+        self.assertEqual(new_grid.locations[OCCUPANTS.P2], (3,1))
+        self.assertCountEqual(new_grid.matrix[(0, 1)].occupants, [])
+        self.assertCountEqual(new_grid.matrix[(1, 1)].occupants, [OCCUPANTS.P1, OCCUPANTS.ATTACKER])
+        self.assertListEqual(new_grid.matrix[(3, 1)].occupants, [OCCUPANTS.P2])
+
+    def test_apply_defender_action(self):
+        grid = Grid()
+        new_grid = grid.apply_defender_action(Actions.NORTH, Actions.SOUTH)
+        self.assertEqual(grid.locations[OCCUPANTS.ATTACKER], (0, 1))
+        self.assertEqual(grid.locations[OCCUPANTS.P1], (1, 1))
+        self.assertEqual(grid.locations[OCCUPANTS.P2], (3, 1))
+        self.assertListEqual(grid.matrix[(0, 1)].occupants, [OCCUPANTS.ATTACKER])
+        self.assertListEqual(grid.matrix[(1, 1)].occupants, [OCCUPANTS.P1])
+        self.assertListEqual(grid.matrix[(3, 1)].occupants, [OCCUPANTS.P2])
+
+        self.assertEqual(new_grid.locations[OCCUPANTS.ATTACKER], (0, 1))
+        self.assertEqual(new_grid.locations[OCCUPANTS.P1], (1, 2))
+        self.assertEqual(new_grid.locations[OCCUPANTS.P2], (3, 0))
+        self.assertCountEqual(new_grid.matrix[(1, 1)].occupants, [])
+        self.assertListEqual(new_grid.matrix[(3, 1)].occupants, [])
+        self.assertCountEqual(new_grid.matrix[(0, 1)].occupants, [ OCCUPANTS.ATTACKER])
+        self.assertCountEqual(new_grid.matrix[(1, 2)].occupants, [OCCUPANTS.P1])
+        self.assertListEqual(new_grid.matrix[(3, 0)].occupants, [OCCUPANTS.P2])
+if __name__ == '__main__':
+    unittest.main()
